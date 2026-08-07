2f694044d0cc158bb4e17c644e8a8cbe4f7efe8e
Rakesh Allam <allamrakesh888@gmail.com>
2026-08-07 15:38:41 +0530

    summarizer : generates title & summary from transcript

---
+++ core/summarizer.py
@@ -0,0 +1,73 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+
+import os 
+
+def get_llm():
+    return ChatOpenAI(model="gpt-4o", temperature=0.5, api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def split_transcript(transcript: str) -> list:
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 3000,
+        chunk_overlap = 200
+    )
+
+    return splitter.split_text(transcript)
+
+def generate_summary(transcript : str) -> str:
+    llm = get_llm()
+
+    map_prompt = ChatPromptTemplate.from_messages(
+        [
+        ("system", "Summarize this portion of a meeting transcript concisely."),
+        ("human", "{text}"),
+    ]
+    )
+
+    map_chain = map_prompt | llm | StrOutputParser()
+
+    chunks = split_transcript(transcript)
+
+    chunk_summaries = [map_chain.invoke({"text" : chunk}) for chunk in chunks]
+
+    combined_text = "\n\n".join(chunk_summaries)
+
+    combined_prompt = ChatPromptTemplate.from_messages(
+        [
+        (
+            "system",
+            "You are an expert youtube audio summarizer. Combine these partial summaries "
+            "into one final professional summary in bullet points.",
+        ),
+        ("human", "{text}"),
+    ]
+    )
+
+    combined_chain = (
+        RunnablePassthrough() | RunnableLambda(lambda x:{"text":x}) | combined_prompt | llm | StrOutputParser()
+    )
+
+    return combined_chain.invoke(combined_text)
+
+def generate_title(transcipt : str) -> str:
+    llm = get_llm()
+
+    title_chain = (
+        RunnablePassthrough() | RunnableLambda(lambda x:{"text":x}) | 
+        ChatPromptTemplate.from_messages([
+             (
+                "system",
+                "Based on the youtube video transcript, generate a short professional video title "
+                "(max 8 words). Only return the title, nothing else.",
+            ),
+            ("human", "{text}"),
+        ])
+        | llm
+        | StrOutputParser()
+    )
+
+    return title_chain.invoke(transcipt[:2000])
